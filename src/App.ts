4bb250e830e16afe8aca24111629fa562ad74c38
Zander Olidan <zander@playstudios.asia>
2020-09-07 19:46:50 +0800

    NOTWORKING

--- src/App.ts
+++ src/App.ts
@@ -1,11 +1,13 @@
 import telegraf, { Telegraf, Extra, Markup } from 'telegraf';
 import { createWordSet, showWordSetAction, checkWord } from './services/GameService';
 import { CREATE_WORD_SET, SHOW_WORD_SET } from './ActionNames';
-import { BOT_TOKEN } from './EnvironmentVariables';
+import { BOT_TOKEN, WEBHOOK_URL } from './EnvironmentVariables';
+import * as fastify from 'fastify';
 
 
 const bot = new Telegraf(BOT_TOKEN);
 bot.use(Telegraf.log())
+bot.telegram.setWebhook(WEBHOOK_URL);
 
 
 bot.command('solo', createWordSet);
@@ -13,7 +15,11 @@ bot.command('test', ctx => ctx.reply('lol'));
 
 bot.action(SHOW_WORD_SET, showWordSetAction);
 bot.on('text', checkWord);
-bot.launch();
+
+
+
+bot.startWebhook('/', {}, 8443);
+// bot.launch();
 
 
 // TODO:
--- src/EnvironmentVariables.ts
+++ src/EnvironmentVariables.ts
@@ -2,8 +2,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 const BOT_TOKEN = process.env.BOT_TOKEN;
 const REDIS_SERVER = process.env.REDIS_SERVER;
+const WEBHOOK_URL = process.env.WEBHOOK_URL;
 
 export {
     BOT_TOKEN,
     REDIS_SERVER,
+    WEBHOOK_URL
 };
