5f0c4ac3eaa646c2b7da1f9e584d58f7ecbe3c95
Zander Olidan <zander@playstudios.asia>
2020-09-08 15:36:20 +0800

    Added cert

--- src/App.ts
+++ src/App.ts
@@ -1,24 +1,39 @@
 import telegraf, { Telegraf, Extra, Markup } from 'telegraf';
 import { createWordSet, showWordSetAction, checkWord } from './services/GameService';
 import { CREATE_WORD_SET, SHOW_WORD_SET } from './ActionNames';
-import { BOT_TOKEN, WEBHOOK_URL, WEBHOOK_PORT, WEBHOOK_ENDPOINT } from './EnvironmentVariables';
+import { BOT_TOKEN, WEBHOOK_URL, WEBHOOK_PORT, WEBHOOK_ENDPOINT, SIGNED_CERT } from './EnvironmentVariables';
 import * as fastify from 'fastify';
 
 
-const bot = new Telegraf(BOT_TOKEN);
-bot.use(Telegraf.log())
-bot.telegram.setWebhook(WEBHOOK_URL);
+
+try {
+    const bot = new Telegraf(BOT_TOKEN);
+    bot.use(Telegraf.log())
+    // bot.telegram.setWebhook(WEBHOOK_URL);
 
 
-bot.command('solo', createWordSet);
-bot.command('test', ctx => ctx.reply('lol'));
+    bot.command('solo', createWordSet);
+    bot.command('test', ctx => ctx.reply('lol'));
 
-bot.action(SHOW_WORD_SET, showWordSetAction);
-bot.on('text', checkWord);
+    bot.action(SHOW_WORD_SET, showWordSetAction);
+    bot.on('text', checkWord);
 
 
-bot.startWebhook(WEBHOOK_ENDPOINT, null, WEBHOOK_PORT);
-// bot.launch();
+    // bot.startWebhook(WEBHOOK_ENDPOINT, null, WEBHOOK_PORT);
+    bot.launch({
+        webhook: {
+            port: WEBHOOK_PORT,
+            domain: WEBHOOK_URL,
+            host: "127.0.0.1",
+            hookPath: WEBHOOK_ENDPOINT,
+            tlsOptions: {
+                cert: SIGNED_CERT
+            }
+        }
+    });
+} catch (e) {
+    console.error(e);
+}
 
 
 // TODO:
--- src/EnvironmentVariables.ts
+++ src/EnvironmentVariables.ts
@@ -5,11 +5,13 @@ const REDIS_SERVER = process.env.REDIS_SERVER;
 const WEBHOOK_URL = process.env.WEBHOOK_URL;
 const WEBHOOK_PORT = process.env.WEBHOOK_PORT ? parseInt(process.env.WEBHOOK_PORT, 10) : 8443;
 const WEBHOOK_ENDPOINT = process.env.WEBHOOK_ENDPOINT;
+const SIGNED_CERT = process.env.SIGNED_CERT;
 
 export {
     BOT_TOKEN,
     REDIS_SERVER,
     WEBHOOK_URL,
     WEBHOOK_PORT,
-    WEBHOOK_ENDPOINT
+    WEBHOOK_ENDPOINT,
+    SIGNED_CERT
 };
