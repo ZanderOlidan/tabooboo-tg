3d864a996961a379997dce9788c16bb40fd27995
Zander Olidan <zander@playstudios.asia>
2020-09-22 14:19:03 +0800

    Changed the bot to use  prod settings

--- src/App.ts
+++ src/App.ts
@@ -3,32 +3,33 @@ import { createWordSet, showWordSetAction, checkWord } from './services/GameServ
 import { SHOW_WORD_SET } from './ActionNames';
 import { BOT_TOKEN, WEBHOOK_URL, WEBHOOK_PORT, WEBHOOK_ENDPOINT, SIGNED_CERT, ENVIRONMENT } from './EnvironmentVariables';
 import { readFileSync } from 'fs';
-try {
-    const bot = new Telegraf(BOT_TOKEN);
-    bot.use(Telegraf.log())
+
+(async () => {
+    try {
+        const bot = new Telegraf(BOT_TOKEN);
+        bot.use(Telegraf.log())
 
 
-    bot.command('solo', createWordSet);
-    bot.command('test', ctx => ctx.reply('lol'));
+        bot.command('solo', createWordSet);
+        bot.command('test', ctx => ctx.reply('lol'));
 
-    bot.action(SHOW_WORD_SET, showWordSetAction);
-    bot.on('text', checkWord);
+        bot.action(SHOW_WORD_SET, showWordSetAction);
+        bot.on('text', checkWord);
 
-    if (ENVIRONMENT.includes('dev')) {
-        bot.telegram.setWebhook(`${WEBHOOK_URL}/bot${BOT_TOKEN}`);
-        bot.startWebhook(WEBHOOK_ENDPOINT, {}, 8443);
-    } else {
-        bot.telegram.setWebhook(`${WEBHOOK_URL}/${WEBHOOK_ENDPOINT}/bot${BOT_TOKEN}`, {
-            source: SIGNED_CERT,
-        }, 100);
-        bot.startWebhook(WEBHOOK_ENDPOINT, {
-            ca: readFileSync(SIGNED_CERT),
-        }, WEBHOOK_PORT, "127.0.0.1");
+        if (ENVIRONMENT.includes('dev')) {
+            bot.telegram.setWebhook(`${WEBHOOK_URL}/bot${BOT_TOKEN}`);
+            bot.startWebhook(WEBHOOK_ENDPOINT, {}, 8443);
+        } else {
+            await bot.telegram.setWebhook(`${WEBHOOK_URL}/${WEBHOOK_ENDPOINT}/bot${BOT_TOKEN}`, {
+                source: readFileSync(SIGNED_CERT),
+            }, 100);
+            bot.startWebhook(`/${WEBHOOK_ENDPOINT}/bot${BOT_TOKEN}`, null, WEBHOOK_PORT, "127.0.0.1");
+        }
+
+    } catch (e) {
+        console.error(e);
     }
-
-} catch (e) {
-    console.error(e);
-}
+})();
 
 
 // TODO:
